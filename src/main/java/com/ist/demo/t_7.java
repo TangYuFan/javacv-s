a643c04a91d4770da2738b24301d35426e3b6d23
13388236293 <tangyufan123>
2019-05-07 00:57:32 +0800

    1.图片的人脸检测          2.视频的人脸检测

---
+++ src/main/java/com/ist/demo/t_7.java
@@ -0,0 +1,96 @@
+package com.ist.demo;
+
+
+import org.bytedeco.javacpp.opencv_core.*;
+import org.bytedeco.javacpp.opencv_objdetect.*;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import javax.swing.*;
+
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_imgproc.equalizeHist;
+
+/**
+ * @desc : 人脸检测
+ * @auth : TYF
+ * @data : 2019-03-08 - 15:33
+ */
+public class t_7 {
+
+    //内存溢出
+    static {
+        System.setProperty("org.bytedeco.javacpp.maxphysicalbytes","0");
+        System.setProperty("org.bytedeco.javacpp.maxbytes","0");
+    }
+
+    //人脸检测
+    public static Mat detectFace(Mat src)
+    {
+        //面部识别级联分类器
+        CascadeClassifier cascade = new CascadeClassifier("E:\\work\\opencv\\opencv-master\\data\\lbpcascades\\lbpcascade_frontalface.xml");
+        //矢量图初始化
+        Mat grayscr=new Mat();
+        //彩图灰度化
+        cvtColor(src,grayscr,COLOR_BGRA2GRAY);
+        //均衡化直方图
+        equalizeHist(grayscr,grayscr);
+        RectVector faces=new RectVector();
+        cascade.detectMultiScale(grayscr, faces);
+        //size就是检测到的人脸个数
+        for(int i=0;i<faces.size();i++)
+        {
+            Rect face_i=faces.get(i);
+            rectangle(src, face_i, new Scalar(0, 0, 255, 1));
+        }
+        //显示释放否则内存溢出
+        grayscr.release();
+        return src;
+    }
+
+    //读取视频
+    public static void frameRecord(String inputFile) throws Exception{
+        //读取视频
+        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFile);
+        try {
+            grabber.start();
+            //new窗口
+            CanvasFrame canvas = new CanvasFrame("视频");
+            canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            canvas.setAlwaysOnTop(true);
+            Frame frame = grabber.grabImage();
+            OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+            while (true) {
+                if(!canvas.isEnabled())
+                {//窗口是否关闭
+                    grabber.stop();
+                    System.exit(0);
+                }
+                //frame转mat
+                Mat scr=converter.convertToMat(frame);
+                //人脸检测
+                detectFace(scr);
+                //mat转frame
+                frame =converter.convert(scr);
+                //按帧显示
+                canvas.showImage(grabber.grabFrame());
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (grabber != null) {
+                grabber.stop();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        //本地视频文件、rtsp地址、rtmp地址
+        String inputFile = "E:\\work\\vlc\\test.mp4";
+        frameRecord(inputFile);
+    }
+
+}
--- src/main/java/com/ist/javacv/t_4.java
+++ src/main/java/com/ist/javacv/t_4.java
@@ -6,8 +6,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+
+import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_imgcodecs;
+import org.bytedeco.javacpp.opencv_objdetect;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
@@ -77,72 +80,124 @@ public class t_4 {
 
 
 
-    public static void main(String args[]){
+//    public static void main(String args[]){
+//
+//            t_4 obj = new t_4();
+//
+//            //以彩色模式读取图像
+//            Mat  image = imread("D:\\1.jpg", IMREAD_COLOR);
+//            if (image==null||image.empty()) {
+//                System.out.println("读取图像失败，图像为空");
+//                return;
+//            }
+//
+//            System.out.println("图像宽x高" + image.cols() + " x " + image.rows());
+//
+//            //显示图像
+//            obj.imShow(image, "原始图像");
+//
+//            //创建空mat，保存处理图像
+//            Mat result = new Mat();
+//            int flipCode=1;
+//
+//            //水平翻转
+//            flip(image, result, flipCode);
+//            //显示处理过的图像
+//            obj.imShow(result, "水平翻转");
+//
+//            //保存图像
+//            obj.imWrite(result, "data/javacv/lakeResult.jpg");
+//
+//            //克隆图像
+//            Mat imageCircle = image.clone();
+//
+//
+//            //图像上画圆
+//            circle(imageCircle, // 目标图像
+//                    new Point(420, 150), // 圆心坐标
+//                    65, // radius
+//                    new Scalar(0,200,0,0), // 颜色，绿色
+//                    2, // 线宽
+//                    8, // 8-connected line
+//                    0); // shift
+//
+//            opencv_imgproc.putText(imageCircle, //目标图像
+//                    "Lake and Tower", // 文本内容(不可包含中文)
+//                    new Point(460, 200), // 文本起始位置坐标
+//                    FONT_HERSHEY_PLAIN, // 字体类型
+//                    2.0, // 字号大小
+//                    new Scalar(0,255,0,3), //文本颜色，绿色
+//                    1, // 文本字体线宽
+//                    8, // 线形.
+//                    false); //控制文本走向
+//            obj.imShow(imageCircle, "画圆mark");
+//
+//
+//            //图像二值化
+//            Mat gray=new Mat();
+//            cvtColor(image,gray,COLOR_RGB2GRAY);		//彩色图像转为灰度图像
+//            obj.imShow(gray, "灰度图像");
+//            Mat bin=new Mat();
+//            threshold(gray,bin,120,255,THRESH_TOZERO); 	//图像二值化
+//            obj.imShow(bin, "二值图像");
+//            waitKey(0);
+//
+//        }
 
-            t_4 obj = new t_4();
 
-            //以彩色模式读取图像
+    //人脸检测
+    public Mat detectFace(Mat src)
+    {
+        //面部识别级联分类器
+        opencv_objdetect.CascadeClassifier cascade = new opencv_objdetect.CascadeClassifier("E:\\work\\opencv\\opencv-master\\data\\lbpcascades\\lbpcascade_frontalface.xml");
+        //矢量图初始化
+        Mat grayscr=new Mat();
+        //彩图灰度化
+        cvtColor(src,grayscr,COLOR_BGRA2GRAY);
+        //均衡化直方图
+        equalizeHist(grayscr,grayscr);
+        opencv_core.RectVector faces=new opencv_core.RectVector();
+        cascade.detectMultiScale(grayscr, faces);
+        //size就是检测到的人脸个数
+        for(int i=0;i<faces.size();i++)
+        {
+            opencv_core.Rect face_i=faces.get(i);
+            rectangle(src, face_i, new Scalar(0, 0, 255, 1));
+        }
+        //显示释放否则内存溢出
+        grayscr.release();
+        return src;
+    }
+
+
+        //用一张图片测试人脸检测
+        public void face(){
+
+            //mat方式读取图片
             Mat  image = imread("D:\\1.jpg", IMREAD_COLOR);
             if (image==null||image.empty()) {
                 System.out.println("读取图像失败，图像为空");
                 return;
             }
 
-            System.out.println("图像宽x高" + image.cols() + " x " + image.rows());
+            //显示图像
+            this.imShow(image, "原始图像");
+
+            //人脸检测
+            this.detectFace(image);
 
             //显示图像
-            obj.imShow(image, "原始图像");
+            this.imShow(image, "识别图像");
 
-            //创建空mat，保存处理图像
-            Mat result = new Mat();
-            int flipCode=1;
-
-            //水平翻转
-            flip(image, result, flipCode);
-            //显示处理过的图像
-            obj.imShow(result, "水平翻转");
-
-            //保存图像
-            obj.imWrite(result, "data/javacv/lakeResult.jpg");
-
-            //克隆图像
-            Mat imageCircle = image.clone();
-
-
-            //图像上画圆
-            circle(imageCircle, // 目标图像
-                    new Point(420, 150), // 圆心坐标
-                    65, // radius
-                    new Scalar(0,200,0,0), // 颜色，绿色
-                    2, // 线宽
-                    8, // 8-connected line
-                    0); // shift
-
-            opencv_imgproc.putText(imageCircle, //目标图像
-                    "Lake and Tower", // 文本内容(不可包含中文)
-                    new Point(460, 200), // 文本起始位置坐标
-                    FONT_HERSHEY_PLAIN, // 字体类型
-                    2.0, // 字号大小
-                    new Scalar(0,255,0,3), //文本颜色，绿色
-                    1, // 文本字体线宽
-                    8, // 线形.
-                    false); //控制文本走向
-            obj.imShow(imageCircle, "画圆mark");
-
-
-            //图像二值化
-            Mat gray=new Mat();
-            cvtColor(image,gray,COLOR_RGB2GRAY);		//彩色图像转为灰度图像
-            obj.imShow(gray, "灰度图像");
-            Mat bin=new Mat();
-            threshold(gray,bin,120,255,THRESH_TOZERO); 	//图像二值化
-            obj.imShow(bin, "二值图像");
-            waitKey(0);
 
         }
 
 
 
+        public static void main(String args[]){
+            t_4 obj = new t_4();
+            obj.face();
+        }
 
 
 
