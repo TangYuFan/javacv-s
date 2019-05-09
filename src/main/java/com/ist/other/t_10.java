62c769212a070922a25162b1613271b8d185d666
TangYuFan <995435702@qq.com>
2019-05-09 17:25:12 +0800

    新增两个测试

---
+++ src/main/java/com/ist/other/t_10.java
@@ -0,0 +1,13 @@
+package com.ist.other;
+
+
+/**
+ * @desc : CNN(卷积神经网络)训练    https://www.cnblogs.com/charlotte77/p/7759802.html
+ * @auth : TYF
+ * @date : 2019-05-08 - 9:52
+ */
+public class t_10 {
+
+
+    //猫脸识别https://www.cnblogs.com/qcloud1001/p/9209725.html
+}
